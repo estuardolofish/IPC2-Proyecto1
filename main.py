53a1114cb1561b35eeacbc9e275db6c419723836
estuardolofish <ellp2038@gmail.com>
2021-08-24 19:41:43 -0600

    opcion 1 de la carga de archivos y escritura en consola

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 import xml.etree.ElementTree as ET
 
 
+def cargarArchivo(ruta):
+    tree = ET.parse(ruta)
+    root = tree.getroot()
 
+    for elemento in root:
+        print('Terreno -> ', elemento.attrib['nombre'])
+        for subelemento in elemento:
+            # para obtener los datos de la dimension
+            for dimension in subelemento.iter('dimension'):
+                for dimM in dimension.iter('m'):
+                    print('m -> ', dimM.text)
+                for dimN in dimension.iter('n'):
+                    print('n -> ', dimN.text)
+                    
+            # para obtener los datos de la posicion inicial        
+            for posicionInicial in subelemento.iter('posicioninicio'):
+                for posXInicio in posicionInicial.iter('x'):
+                    print('x Inicio -> ', posXInicio.text)
+                for posYInicio in posicionInicial.iter('y'):
+                    print('y Inicio -> ', posYInicio.text)
 
+            # para obtener los datos de la posicion final        
+            for posicionFinal in subelemento.iter('posicionfin'):
+                for posXFinal in posicionFinal.iter('x'):
+                    print('x Fin -> ', posXFinal.text)
+                for posYFinal in posicionFinal.iter('y'):
+                    print('y Fin -> ', posYFinal.text)
 
+            for posicion in subelemento.iter('posicion'):
+                print('cordenadas -> ', 'x=' ,posicion.attrib['x'] , ' y=', posicion.attrib['y'], ' - ', posicion.text )
+                # print('Gasolina -> ',posicion.text)
+                # for cordenadaX in posicion.attrib['x']:
+                #     print('x cordenada -> ',cordenadaX.text)
+
+                
 def Menu():
     opcion = 0
     while opcion != 6:
@@ -17,7 +49,9 @@ def Menu():
         opcion = input("-> ")
         ruta = ''
         if opcion == '1':
-            print('Ingrese nombre fichero')
+            Filename = input('Ingrese nombre de archivo:')
+            file = './' + Filename
+            cargarArchivo(file)
  
         elif opcion == '2':
             print('Ingrese nombre fichero')
