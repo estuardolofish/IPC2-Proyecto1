64d300cb683d8b3e7cf3d03c4698250ac355fd08
estuardolofish <ellp2038@gmail.com>
2021-08-26 20:09:52 -0600

    prueba

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def generarGrafica(terreno, coordenadas):
         } }
     '''
 
-    
+    fdsa
     miArchivo = open(str(terreno)+'.dot', 'w')
     miArchivo.write(grafica)
     miArchivo.close()
