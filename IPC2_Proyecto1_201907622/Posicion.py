c246fc6da22ae57fd385871070afc410f65352d6
estuardolofish <31548999+estuardolofish@users.noreply.github.com>
2021-08-26 19:27:18 -0600

    Delete IPC2_Proyecto1_201907622 directory

--- IPC2_Proyecto1_201907622/Posicion.py
+++
@@ -1,7 +0,0 @@
-class Posicion:
-    def __init__(self, x, y, gas):
-        self.x = x
-        self.y = y
-        self.gas = gas
-        self.siguiente = None
-        self.anterior = None
--- IPC2_Proyecto1_201907622/README.md
+++
@@ -1,1 +0,0 @@
-# IPC2-Proyecto1
